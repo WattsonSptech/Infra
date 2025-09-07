766dc5169ae5dd3e362b943695da0c7c47c124de
PereiraGus <gustavo.oito@hotmail.com>
2025-09-07 18:56:02 -0300

    Implementação inicial de lambda para S3

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,16 +1,25 @@
 resource "aws_lambda_function" "lbd_data_dynamodb" {
   function_name = "lmb_data_dynamodb"
-  role          = "arn:aws:iam::716961619224:role/LabRole"
+  role          = "arn:aws:iam::637952174709:role/LabRole"
   handler       = "lmb_data_dynamodb.lambda_handler"
   runtime       = "python3.10"
   filename      = "../script_lambda/lmb_data_dynamodb.zip"
-  timeout       = 30
+  timeout       = 306
 
   environment {
     variables = {
       TABLE_NAME = var.dynamodb_table_wattson_name
     }
   }
+}
+
+resource "aws_lambda_function" "lbd_data_s3" {
+  function_name = "lmb_data_s3"
+  role          = "arn:aws:iam::637952174709:role/LabRole"
+  handler       = "lmb_data_s3.lambda_handler"
+  runtime       = "python3.10"
+  filename      = "../script_lambda/lmb_data_s3.zip"
+  timeout       = 306
 }
 
 # resource "aws_lambda_function" "lbd-files-s3" {
